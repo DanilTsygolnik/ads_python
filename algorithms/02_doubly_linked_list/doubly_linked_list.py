e8f662c4a2db17c84631f448aa8a349bdcc47a57
Danil Tsygolnik <tsygolnikd@gmail.com>
2021-10-31 11:47:52 +0300

    algo, doubly linked list, wip: main tasks and test done; ready for approval

--- algorithms/02_doubly_linked_list/doubly_linked_list.py
+++ algorithms/02_doubly_linked_list/doubly_linked_list.py
@@ -178,4 +178,9 @@ class LinkedList2:
                     node = node.next
 
     def add_in_head(self, newNode):
-        pass
+        if self.head is None:
+            self.tail = newNode
+        else:
+            self.head.prev = newNode
+            newNode.next = self.head
+        self.head = newNode
