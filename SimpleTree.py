7ece29c42b57b7dc0c75c5e393989bd617d12bf6
Danil Tsygolnik <tsygolnikd@gmail.com>
2022-02-10 17:00:58 +0300

    simple tree: classes templates

---
+++ SimpleTree.py
@@ -0,0 +1,32 @@
+class SimpleTree:
+
+    def __init__(self, Node=None):
+        self.root = Node
+
+
+    def AddChild(self, ParentNode, NewChild):
+        pass
+
+
+    def DeleteNode(self, NodeToDelete):
+        pass
+
+
+    def GetAllNodes(self):
+        pass
+
+
+    def FindNodesByValue(self, val):
+        pass
+
+
+    def MoveNode(self, OriginalNode, NewParent):
+        pass
+
+
+    def Count(self):
+        pass
+
+
+    def LeafCount(self):
+        pass
--- SimpleTreeNode.py
+++ SimpleTreeNode.py
@@ -1,23 +1,4 @@
 class SimpleTreeNode:
-	"""
-    
-    Attributes
-    ----------
-    leaf_id : ??
-        идентификатор листа, чтобы можно было ссылаться
-    leaf_value : None, int, optional
-        значение, которое хранится в листе (default is None)
-    parent : SimpleTreeNode.leaf_id
-        указатель на родительский узел
-        желателен для быстрого выполнения различных операций
-    children : []
-        список дочерних узлов
-    leaf_lvl : int
-        поддержка уровня узлов без анализа всего дерева (extra task)
 
-    """
-    def __init__(self, val, parent):
-        self.NodeValue = val # значение в узле
-        self.Parent = parent # родитель или None для корня
-        self.Children = [] # список дочерних узлов
-	
+    def __init__(self, val=None, parent):
+        pass
