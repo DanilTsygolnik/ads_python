af18d8f3eff0489806ac6273b7a81abb11aba7f2
Danil Tsygolnik <tsygolnikd@gmail.com>
2022-02-27 20:03:41 +0300

    Finished in 13 hours 30 minutes

---
+++ 06_BalancedBST/BalancedBST.py
@@ -0,0 +1,52 @@
+class BSTNode:
+    def __init__(self, key, parent):
+        self.NodeKey = key
+        self.Parent = parent
+        self.LeftChild = None
+        self.RightChild = None
+        self.Level = 0
+
+class BalancedBST:
+    def __init__(self):
+    	self.Root = None
+
+    def GenerateBST(self, node, arr):
+        if len(arr):
+            i = (len(arr)-1)//2
+            if self.Root is None:
+                self.Root = BSTNode(arr[i], None)
+                self.Root.Level = 1
+                node = self.Root
+            else:
+                if arr[i] < node.NodeKey:
+                    node.LeftChild = BSTNode(arr[i], node)
+                    node = node.LeftChild
+                else:
+                    node.RightChild = BSTNode(arr[i], node)
+                    node = node.RightChild
+                node.Level += node.Parent.Level + 1
+            self.GenerateBST(node, arr[:i])
+            self.GenerateBST(node, arr[i+1:])
+
+    def GenerateTree(self, a):
+        a = sorted(a)
+        self.GenerateBST(self.Root, a)
+    
+    def TreeDepth(self, node):
+        if node is None:
+            return 0
+        left_depth = self.TreeDepth(node.LeftChild)
+        right_depth = self.TreeDepth(node.RightChild)
+        return max(left_depth, right_depth) + 1
+
+    def IsBalanced(self, root_node):
+        if root_node is not None:
+            left = self.TreeDepth(root_node.LeftChild)
+            right = self.TreeDepth(root_node.RightChild)
+            if abs(left-right) > 1:
+                return False
+            if root_node.LeftChild is not None:
+                return self.IsBalanced(root_node.LeftChild)
+            if root_node.RightChild is not None:
+                return self.IsBalanced(root_node.RightChild)
+        return True
