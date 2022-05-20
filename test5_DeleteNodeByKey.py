c60a28a6d8d2ecab55fee0402fda3b3f69d78d36
Danil Tsygolnik <tsygolnikd@gmail.com>
2022-05-20 14:30:49 +0300

    BST: DeleteNodeByKey() tests (wip)

--- test5_DeleteNodeByKey.py
+++ test5_DeleteNodeByKey.py
@@ -6,121 +6,182 @@ class TestBSTNode_replace_with(unittest.TestCase):
 
     def setUp(self):
         self.nodeA = BSTNode('A', 'A', None)
+        self.nodeB = BSTNode('B', 'B', None)
         self.node1 = BSTNode(1, 1, None)
         self.node2 = BSTNode(2, 2, None)
+        self.node3 = BSTNode(3, 3, None)
+        self.node4 = BSTNode(4, 4, None)
+        self.node5 = BSTNode(5, 5, None)
+        self.node6 = BSTNode(6, 6, None)
 
     def test1(self):
         '''
-        Узел nodeA - родитель узла node1.
-        Узел node1 - наследник nodeA слева.
+        Узел nodeA - корень.
+        Наследники:
+          - nodeA.LeftChild = None
+          - nodeA.RightChild = node1
+
+        Узел node1 - наследник nodeA справа:
+        Наследники:
+          - node1.LeftChild = node2
+          - node1.RightChild = node3
+
         Задача: удалить узел node1 из связки с nodeA (замена на None).
         '''
-        self.nodeA.LeftChild = self.node1
-        self.node1.Parent = self.nodeA
-        
-        self.node1.replace_with(None)
-        self.assertIs(self.nodeA.LeftChild, None)
-        self.assertIs(self.node1.Parent, None)
-
-    def test2(self):
-        '''
-        Узел nodeA - родитель узла node1.
-        Узел node1 - наследник nodeA слева.
-        Узел node2 - orphan.
-        Задача: заменить узел node1 в связке с NodeA на node2.
-        '''
         # nodeA--node1
-        self.nodeA.LeftChild = self.node1
-        self.node1.Parent = self.nodeA
-        
-        self.node1.replace_with(self.node2)
-        # nodeA--node2
-        self.assertIs(self.nodeA.LeftChild, node2)
-        self.assertIs(self.node2.Parent, nodeA)
-        # node1 became orphan
-        self.assertIs(self.node1.Parent, None)
-        self.assertIs(self.node1.LeftChild, None)
-
-    def test3(self):
-        '''
-        Узел nodeA - родитель узла node1.
-        Узел node1 - наследник nodeA слева.
-        Узел node2 - наследник node1 слева.
-        Задача: заменить узел node1 в связке с NodeA на node2.
-        '''
-        # nodeA--node1
-        self.nodeA.LeftChild = self.node1
+        self.nodeA.RightChild = self.node1
         self.node1.Parent = self.nodeA
         # node1--node2
         self.node1.LeftChild = self.node2
         self.node2.Parent = self.node1
-        
-        self.node1.replace_with(self.node2)
-        # nodeA--node2
-        self.assertIs(self.nodeA.LeftChild, node2)
-        self.assertIs(self.node2.Parent, nodeA)
-        # node1 became orphan
+        # node1--node3
+        self.node1.RightChild = self.node3
+        self.node3.Parent = self.node1
+
+        self.node1.replace_with(None)
+        self.assertIs(self.nodeA.RightChild, None)
+        self.assertIs(self.node1.Parent, None)
+        self.assertIs(self.node1.LeftChild, self.node2)
+        self.assertIs(self.node1.RightChild, self.node3)
+
+    def test2(self):
+        '''
+        Узел nodeA - корень.
+        Наследники:
+          - nodeA.LeftChild = None
+          - nodeA.RightChild = node1
+
+        Узел node1 - наследник nodeA справа:
+        Наследники:
+          - node1.LeftChild = node2
+          - node1.RightChild = node3
+
+        Узел nodeB - корень.
+        Наследники:
+          - nodeB.LeftChild = node4
+          - nodeB.RightChild = None
+
+        Узел node4 - наследник nodeB слева:
+        Наследники:
+          - node4.LeftChild = node5
+          - node4.RightChild = node6
+
+        Задача: заменить узел node1 на node4.
+        '''
+        # nodeA--node1
+        self.nodeA.RightChild = self.node1
+        self.node1.Parent = self.nodeA
+        # node1--node2
+        self.node1.LeftChild = self.node2
+        self.node2.Parent = self.node1
+        # node1--node3
+        self.node1.RightChild = self.node3
+        self.node3.Parent = self.node1
+
+        # nodeB--node4
+        self.nodeB.LeftChild = self.node4
+        self.node4.Parent = self.nodeB
+        # node4--node5
+        self.node4.LeftChild = self.node5
+        self.node5.Parent = self.node4
+        # node4--node6
+        self.node4.RightChild = self.node6
+        self.node6.Parent = self.node4
+
+        self.node1.replace_with(self.node4)
+        self.assertIs(self.nodeA.RightChild, self.node4)
+        self.assertIs(self.node4.Parent, self.nodeA)
+        self.assertIs(self.node1.Parent, None)
+        self.assertIs(self.nodeB.LeftChild, None)
+
+
+    def test3(self):
+        '''
+        Узел nodeA - корень.
+        Наследники:
+          - nodeA.LeftChild = None
+          - nodeA.RightChild = node1
+
+        Узел node1 - наследник nodeA справа:
+        Наследники:
+          - node1.LeftChild = node2
+          - node1.RightChild = node3
+
+        Узел node3 - наследник node1 справа:
+        Наследники:
+          - node3.LeftChild = None
+          - node3.RightChild = node4
+
+        Узел node4 - наследник node3 справа.
+        Наследники:
+          - node4.LeftChild = node5
+          - node4.RightChild = node6
+
+        Задача: заменить узел node1 на node4.
+        '''
+        # nodeA--node1
+        self.nodeA.RightChild = self.node1
+        self.node1.Parent = self.nodeA
+        # node1--node2
+        self.node1.LeftChild = self.node2
+        self.node2.Parent = self.node1
+        # node1--node3
+        self.node1.RightChild = self.node3
+        self.node3.Parent = self.node1
+
+        # node3--node4
+        self.node3.RightChild = self.node4
+        self.node4.Parent = self.node3
+        # node4--node5
+        self.node4.LeftChild = self.node5
+        self.node5.Parent = self.node4
+        # node4--node6
+        self.node4.RightChild = self.node6
+        self.node6.Parent = self.node4
+
+        self.node1.replace_with(self.node4)
+        self.assertIs(self.nodeA.RightChild, self.node4)
+        self.assertIs(self.node4.Parent, self.nodeA)
+        self.assertIs(self.node1.Parent, None)
+        self.assertIs(self.node3.LeftChild, None)
+        self.assertIs(self.node3.RightChild, None)
+
+    def test4(self):
+        '''
+        Узел nodeA - корень.
+        Наследники:
+          - nodeA.LeftChild = None
+          - nodeA.RightChild = node1
+
+        Узел node1 - наследник nodeA справа:
+        Наследники:
+          - node1.LeftChild = node2
+          - node1.RightChild = node3
+
+        Задача: заменить узел node1 на node3 и передать
+                наследников node1 узлу node3.
+        '''
+        # nodeA--node1
+        self.nodeA.RightChild = self.node1
+        self.node1.Parent = self.nodeA
+        # node1--node2
+        self.node1.LeftChild = self.node2
+        self.node2.Parent = self.node1
+        # node1--node3
+        self.node1.RightChild = self.node3
+        self.node3.Parent = self.node1
+
+        self.node1.replace_with(self.node3, push_kids=True)
+        self.assertIs(self.nodeA.RightChild, self.node3)
+        self.assertIs(self.node3.Parent, self.nodeA)
+        self.assertIs(self.node3.LeftChild, self.node2)
+        self.assertIs(self.node3.RightChild, None)
         self.assertIs(self.node1.Parent, None)
         self.assertIs(self.node1.LeftChild, None)
+        self.assertIs(self.node1.RightChild, None)
 
 
-'''
-Тест 4
-_____
-Заменить узел n1 с родителем (n1.Parent=nodeA; nodeA.LeftChild=n1) на узел n2 c родителем (n2.Parent.RightChild=nodeB; nodeB.RightChild=n2)
-В итоге (на бумаге ок):
-nodeA.LeftChild=n2
-nodeB.RightChild=None
-n2.Parent=nodeA
-n1.Parent=None
-```
-
-
-class TestDeleteNodeByKey(unittest.TestCase):
-
-    def test1_empty_tree_root_none(self):
-        empty_tree = BST(None)
-        self.assertEqual(empty_tree.Count(), 0)
-
-    def test2_root_only(self):
-        node10 = BSTNode(10, 10, None)
-        tree = BST(node10)
-        self.assertEqual(tree.Count(), 1)
-
-    def test3_two_nodes(self):
-        root = BSTNode(10, 10, None)
-        node6 = BSTNode(6, 6, root)
-        root.LeftChild = node6
-        tree_child_at_left = BST(root)
-        self.assertEqual(tree_child_at_left.Count(), 2)
-
-        node14 = BSTNode(14, 14, root)
-        root.LeftChild = None
-        root.RightChild = node14
-        tree_child_at_right = BST(root)
-        self.assertEqual(tree_child_at_right.Count(), 2)
-
-    def test_4(self):
-        root = BSTNode(10, 10, None)
-        node6 = BSTNode(6, 6, root)
-        node14 = BSTNode(14, 14, root)
-        root.LeftChild = node6
-        root.RightChild = node14
-        tree = BST(root)
-        self.assertEqual(tree.Count(), 3)
-
-    def test_5(self):
-        root = BSTNode(10, 10, None)
-        node2 = BSTNode(2, 2, root)
-        node14 = BSTNode(14, 14, root)
-        root.LeftChild = node2
-        root.RightChild = node14
-        node6 = BSTNode(6, 6, node2)
-        node2.RightChild = node6
-        node8 = BSTNode(8, 8, node6)
-        node6.RightChild = node8
-        tree = BST(root)
-        self.assertEqual(tree.Count(), 5)
+#class TestDeleteNodeByKey(unittest.TestCase):
 
 
 if __name__=="__main__":
